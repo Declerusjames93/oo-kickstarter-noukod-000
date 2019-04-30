ef52e10fd1309d7eaf7e5338b18785501e5d9b5b
James Declerus <james.declerus@esih.edu>
2019-04-30 14:04:36 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,11 +1,11 @@
 class Project
   attr_reader :title, :backers
-  
+
      def initialize(title)
       @title = title
       @backers = []
     end
-  
+
      def add_backer(backer)
       backers << backer
       backer.backed_projects << self
